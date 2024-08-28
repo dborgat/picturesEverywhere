b487b73205f5dbeb47227e2dc24f14adf2282d27
dborgat <dborgat@gmail.com>
2024-08-28 19:22:11 -0300

    feat: update main container styles and add camera button

--- app/index.styles.ts
+++ app/index.styles.ts
@@ -1,11 +1,21 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    mainContainer: {
-        flex: 1,
-        alignItems: 'center',
+    mainContainer: { flex: 1, padding: 20 },
+    flatListColumnWrapper: { justifyContent: "space-between" },
+    flatListContentContainer: { paddingBottom: 5 },
+    itemSeparator: { height: 10 },
+    cameraButtonStyle: {
+        backgroundColor: '#2f6ec1',
+        borderRadius: 5,
+        flexDirection: 'row',
+        padding: 5,
+        width: '100%',
+        gap: 20,
         justifyContent: 'center',
+        alignItems: 'center',
     },
+    cameraButtonText: { fontSize: 20, fontWeight: 'bold' },
 });
 
 export default styles;
